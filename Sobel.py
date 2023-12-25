0eb9cd96d88aab28b6819d283a20becb3b5350ce
Дмитрий Дорошенко <103374133+DmitriiDo@users.noreply.github.com>
2023-12-25 23:44:15 +0300

    Add files via upload

---
+++ Sobel.py
@@ -0,0 +1,37 @@
+# Программа сделана в CoLab
+import requests
+from PIL import Image
+import io
+import numpy as np
+from scipy import ndimage
+import matplotlib.pyplot as plt
+
+image_url = "https://a.d-cd.net/hHtWFkLlBEKorL37Cxl2NCSX6Ns-960.jpg"
+
+response = requests.get(image_url)
+image = Image.open(io.BytesIO(response.content)).convert('L')  # Конвертируем в черно-белое
+
+image_np = np.array(image).astype('int32')
+
+sobel_h = ndimage.sobel(image_np, 0)  # горизонтальный градиент
+sobel_v = ndimage.sobel(image_np, 1)  # вертикальный градиент
+
+magnitude = np.sqrt(sobel_h**2 + sobel_v**2)
+magnitude *= 255.0 / np.max(magnitude) 
+
+fig, axs = plt.subplots(2, 2, figsize=(8, 8))
+
+plt.gray()
+
+axs[0, 0].imshow(image_np)
+axs[0, 1].imshow(sobel_h)
+axs[1, 0].imshow(sobel_v)
+axs[1, 1].imshow(magnitude)
+
+titles = ["Оригинал", "Горизонтальный", "Вертикальный", " "]
+
+for i, ax in enumerate(axs.ravel()):
+    ax.set_title(titles[i])
+    ax.axis("off")
+
+plt.show()
